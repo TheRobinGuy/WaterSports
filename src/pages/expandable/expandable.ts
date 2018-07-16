6152604c1a02cf5024318d5fa13a669f58b5cffa
Glenn Gilmartin <ggilmartin@overstock.com>
2018-07-16 16:46:08 +0100

    Accordian Working

--- src/pages/expandable/expandable.ts
+++ src/pages/expandable/expandable.ts
@@ -9,13 +9,28 @@ import { IonicPage, NavController } from 'ionic-angular';
 export class ExpandablePage {
 
   dives = [
-    { date: "01 Jan 2018", location: "Mullaghmore, Sligo." },
-    { date: "05 Jan 2018", location: "Mullaghmore, Sligo." },
-    { date: "23 Mar 2018", location: "Cassan Snd, Donegal." },
-    { date: "24 Mar 2018", location: "St. Johns Pt, Donegal." },
-   ];
+    { date: "01 Jan 2018", location: "Mullaghmore, Sligo.", hidden: false },
+    { date: "05 Jan 2018", location: "Mullaghmore, Sligo.", hidden: true },
+    { date: "23 Mar 2018", location: "Cassan Snd, Donegal.", hidden: true },
+    { date: "24 Mar 2018", location: "St. Johns Pt, Donegal.", hidden: true },
+  ];
 
   constructor(public navCtrl: NavController) {
+
+  }
+
+  expandItem(item) {
+
+    this.dives.map((listItem) => {
+
+      if (item == listItem) {
+        listItem.hidden = !listItem.hidden;
+        console.log("Hidden : " + listItem.hidden)
+      } else {
+        listItem.hidden = true;
+      }
+      return listItem;
+    });
 
   }
 }
