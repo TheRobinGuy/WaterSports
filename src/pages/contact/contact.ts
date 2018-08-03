1e47d2ef83f0c58f8053ca43181639c169c4a771
Glenn Gilmartin <ggilmartin@overstock.com>
2018-08-03 11:54:35 +0100

    Added cordova geolocation plugin

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -8,7 +8,21 @@ import { NavController } from 'ionic-angular';
 export class ContactPage {
 
   constructor(public navCtrl: NavController) {
-
+    navigator.geolocation.getCurrentPosition(this.onSuccess, this.onError);
   }
+
+  onSuccess(position) {
+    var element = document.getElementById('geolocation');
+    element.innerHTML = 'Latitude: '  + position.coords.latitude      + '<br />' +
+                        'Longitude: ' + position.coords.longitude     + '<br />' +
+                        '<hr />'      + element.innerHTML;
+}
+ 
+
+    onError(error) {
+      alert('code: '    + error.code    + '\n' +
+            'message: ' + error.message + '\n');
+  }
+ 
 
 }
