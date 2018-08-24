31cab7a312b88a86ef7060fd8e72dc20e726454b
Glenn Gilmartin <ggilmartin@overstock.com>
2018-08-24 12:52:25 +0100

    Signup working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { App, MenuController, Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
 import { TabsPage } from '../pages/tabs/tabs';
 import { LoginPage } from '../pages/login/login';
 import { AuthService } from '../services/auth.services';
@@ -14,10 +13,33 @@ export class MyApp {
   // rootPage:any = TabsPage;
   rootPage:any = LoginPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, authService: AuthService) {
+  private app;
+  private nav;
+	private platform;
+	private menu: MenuController;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
+    private auth: AuthService, 
+    app: App,
+    menu: MenuController) {
     platform.ready().then(() => {
+      this.menu = menu;
+		  this.app = app;
+		  this.platform = platform;
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  login() {
+    this.menu.close();
+    this.auth.signOut();
+    this.nav.setRoot(LoginPage);
+  }
+
+  logout() {
+    this.menu.close();
+    this.auth.signOut();
+    this.nav.setRoot(TabsPage);
   }
 }
--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -18,9 +18,21 @@ export class AuthService {
 		return this.afAuth.auth.signInWithEmailAndPassword(credentials.email,
 			 credentials.password);
     }
-    
+
     signUp(credentials) {
         return this.afAuth.auth.createUserWithEmailAndPassword(credentials.email, credentials.password);
     }
+
+    get authenticated(): boolean {
+        return this.user !== null;
+    }
+
+    getEmail() {
+        return this.user && this.user.email;
+    }
+
+    signOut(): Promise<void> {
+        return this.afAuth.auth.signOut();
+      }
 
 }
